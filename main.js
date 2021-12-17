222b2973dc776ab47853fc80fdd79df3eb4ba2d5
Metin <metinmayi@gmail.com>
2021-12-17 18:19:00 +0100

    Connected the MongoDB to the bot

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require("discord.js");
+//#region Requires & Logins
 require("dotenv").config();
-
-// Create a new client instance
+const { assert } = require("console");
+// Discord Section
+const { Client, Intents } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// Login to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
-console.log("Up and running!");
+client.once("ready", () => {
+	console.log("Wiseguy is online!");
+});
+//MongoDB section
+const { MongoClient } = require("mongodb");
+const mongoClient = new MongoClient(process.env.MONGODB_TOKEN);
+mongoClient.connect((error, success) => {
+	console.log("Database is connected!");
+});
+
+//#endregion
