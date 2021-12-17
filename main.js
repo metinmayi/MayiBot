8d797d90029cdd7423215b66e064229158746680
Metin <metinmayi@gmail.com>
2021-12-17 19:07:18 +0100

    Installed all modules and settings for discord slash commands

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 //#region Requires & Logins
 require("dotenv").config();
-const { assert } = require("console");
 // Discord Section
-const { Client, Intents } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { Client, Intents, Interaction } = require("discord.js");
+const client = new Client({
+	intents: [
+		"GUILD_MESSAGES",
+		"GUILD_MEMBERS",
+		"GUILDS",
+		"GUILD_MEMBERS",
+		"GUILD_INTEGRATIONS",
+		"GUILD_PRESENCES",
+		"GUILD_MESSAGE_REACTIONS",
+		"GUILD_MESSAGE_TYPING",
+		"DIRECT_MESSAGES",
+		"DIRECT_MESSAGE_REACTIONS",
+		"DIRECT_MESSAGE_TYPING",
+	],
+});
 client.login(process.env.DISCORD_TOKEN);
 client.once("ready", () => {
 	console.log("Wiseguy is online!");
