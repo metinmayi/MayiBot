4228955b0de52f80114f94c9c959a3db056698cd
Metin <metinmayi@gmail.com>
2021-12-18 18:09:19 +0100

    Created a Message Embed that displays when /commandlist is used

--- commands/commandList.js
+++ commands/commandList.js
@@ -3,11 +3,39 @@ const {
 	SlashCommandBuilder,
 } = require("@discordjs/builders");
 
+const { MessageEmbed } = require("discord.js");
+
+//Commandlist embed
+const embed = {
+	title: "Wiseguy's commands",
+	description: "Below you can find a list of all my commands, enjoy!",
+	url: "",
+	color: 11137603,
+	footer: {
+		icon_url: "",
+		text: "I have not created any of the APIs used for this bot.",
+	},
+	thumbnail: {
+		url: "https://gcdn.pbrd.co/images/u1P2JNb317nx.jpg?o=1",
+	},
+	author: {
+		name: "Metin Mayi",
+		url: "https://github.com/metinmayi",
+		icon_url: "https://avatars.githubusercontent.com/u/90765914?v=4",
+	},
+	fields: [
+		{
+			name: "/joke",
+			value: "Fetches a random joke from an API.",
+		},
+	],
+};
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("commandlist")
 		.setDescription("Gives you a list of my available commands!"),
 	async execute(interaction) {
-		await interaction.reply(exampleEmbed);
+		await interaction.reply({ embeds: [embed] });
 	},
 };
