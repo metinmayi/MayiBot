e73ff96f91e6ffc6f3ad8922509d10dee10f5eda
Metin <metinmayi@gmail.com>
2021-12-20 00:09:59 +0100

    Updated the welcomeMessage's embed

--- modules/welcomeMessage.js
+++ modules/welcomeMessage.js
@@ -1,36 +1,4 @@
 const { MessageEmbed } = require("discord.js");
-const embed = {
-	title: "User profile",
-	description: "Information about your profile.",
-	url: "",
-	color: 11137603,
-	footer: {
-		icon_url: "",
-		text: "I have not created any of the APIs used for this bot.",
-	},
-	thumbnail: {
-		url: "https://gcdn.pbrd.co/images/u1P2JNb317nx.jpg?o=1",
-	},
-	author: {
-		name: "Metin Mayi",
-		url: "https://github.com/metinmayi",
-		icon_url: "https://avatars.githubusercontent.com/u/90765914?v=4",
-	},
-	fields: [
-		{
-			name: "Level: 1",
-			value: "Gain levels through interacting with with the bot.",
-		},
-		{
-			name: "ID",
-			value: "31232132141512512",
-		},
-		{
-			name: "Rank",
-			value: "Novice",
-		},
-	],
-};
 
 async function welcomeMessage(mongoClient, member) {
 	const userListing = await mongoClient
@@ -58,7 +26,7 @@ async function welcomeMessage(mongoClient, member) {
 		fields: [
 			{
 				name: `Level: ${userListing.level}`,
-				value: "Gain levels through interacting with the bot.",
+				value: "Gain experience through interacting with the bot.",
 			},
 			{
 				name: "Experience",
